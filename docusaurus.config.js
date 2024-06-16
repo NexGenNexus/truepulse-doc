2081d5c49bac428fbcd0a24d785247449d9a9bfa
Behfar Khosravi <42641836+behfarkhosravi@users.noreply.github.com>
2024-06-16 03:24:08 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/TruePulse-LAST.png',
 
   // Set the production url of your site here
-  url: 'https://NexGenNexus.github.io',
+  url: 'https://NexGenNexus.github.io/truepulse-doc',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: process.env.NODE_ENV === "production" ? "/truepulse-doc/" : "/",
